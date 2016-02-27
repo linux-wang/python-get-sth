80efb69756843f9fcc3e1c5f2d26d41d4fcd318a
bug-wang <wsw1990wsw@gmail.com>
2016-02-27 22:38:25 +0800

    update youdao.py

--- youdao.py
+++ youdao.py
@@ -5,19 +5,26 @@
 
 import urllib2
 import sys
+import json
 
 
 
-def translate(url,word):
-	html=urllib2.urlopen(url+word)
+def translate(url, word):
+	html = urllib2.urlopen(url+word)
 #	print "result:"+html.read()
-	result=html.read()
-	print result
+	result = html.read()
+	js = json.loads(result)
+	for item in js:
+		if item=="translation":
+			translation = js[item]
+		else:
+			pass
+	print "translation:", translation
 
 def main():
-	url="http://fanyi.youdao.com/openapi.do?keyfrom=oldfriends-wang&key=1908356382&type=data&doctype=json&version=1.1&q="
-	word=raw_input("word:")
-	translate(url,word)
+	url = "http://fanyi.youdao.com/openapi.do?keyfrom=oldfriends-wang&key=1908356382&type=data&doctype=json&version=1.1&q="
+	word = raw_input("word:")
+	translate(url, word)
 
 
 if __name__=="__main__":
